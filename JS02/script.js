e608ffcf3c6f643714fda5dd65d1e45ea1244a73
Prof. Hebert <hebertphp@gmail.com>
2025-02-24 11:49:54 -0300

    Add files via upload

---
+++ JS02/script.js
@@ -0,0 +1,6 @@
+document.write("<h3>Linha em JS externo</h3>");
+document.write("Outra linha em JS externo<br>");
+document.write("Próxima <b>Linha</b> em JS externo");
+alert("E agora?");
+alert("Linha1\nLinha2");
+console.log("Onde \nestou?");
