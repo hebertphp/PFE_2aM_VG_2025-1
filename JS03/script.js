40fcb9d9c96d9a685b8322351d3e661592af6f0c
Prof. Hebert <hebertphp@gmail.com>
2025-02-24 11:49:03 -0300

    Add files via upload

---
+++ JS03/script.js
@@ -0,0 +1,8 @@
+document.write("<h2>Agora vai</h2>");
+
+document.getElementById("saida").innerHTML="<b>Agora foi</b>";
+document.getElementById("saida").innerHTML+="<mark>Estou marcado </mark>";
+
+document.querySelector("h1").innerHTML+=" - Javascript";
+document.querySelector("h1").style.color="blue";
+
---
+++ JS03/script1.js
@@ -0,0 +1,7 @@
+let nota=7.8;
+let nome="Hebert";
+let saida=document.getElementById("saida");
+nota=8.5;
+saida.innerHTML=nome+", você tirou "+nota;
+
+//Hebert, você tirou 7.8
